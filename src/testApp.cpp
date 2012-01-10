8425891f5506bce524df4c1373703f7bf2a7b223
anthony <atran.dummy@gmail.com>
2012-01-09 18:23:55 -0600

    debug edits

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -1,21 +1,5 @@
-//	Cámara Lúcida
+//	Using Camara Lucida's framework
 //	www.camara-lucida.com.ar
-//
-//	Copyright (C) 2011  Christian Parsons
-//	www.chparsons.com.ar
-//
-//	This program is free software: you can redistribute it and/or modify
-//	it under the terms of the GNU General Public License as published by
-//	the Free Software Foundation, either version 3 of the License, or
-//	(at your option) any later version.
-//
-//	This program is distributed in the hope that it will be useful,
-//	but WITHOUT ANY WARRANTY; without even the implied warranty of
-//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//	GNU General Public License for more details.
-//
-//	You should have received a copy of the GNU General Public License
-//	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "testApp.h"
 
@@ -125,7 +109,7 @@ void testApp::render_texture(ofEventArgs &args)
 		contourFinder.findContours(grayImg, 20, (340*240)/1, 10, true);	// find holes
 		
 		grayImg.draw(0,h/2,w/2,h);
-		contourFinder.draw(0,h/2,w/2,h);
+		contourFinder.draw(0,h/2,w/2,h);c
 		
 		
 		//kinect.getDepthTextureReference().draw(0, 0, w, h);
